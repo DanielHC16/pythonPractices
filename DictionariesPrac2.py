e290d31749e659f83819a0ad359da11a1228d15e
Daniel Camacho <danielcamacho0416@gmail.com>
2024-11-27 00:14:07 +0800

    Added extra dictionary

--- DictionariesPrac2.py
+++ DictionariesPrac2.py
@@ -2,8 +2,8 @@
 europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
            'france': { 'capital':'paris', 'population':66.03 },
            'germany': { 'capital':'berlin', 'population':80.62 },
-           'norway': { 'capital':'oslo', 'population':5.084 } }
-
+           'norway': { 'capital':'oslo', 'population':5.084 },
+            'fantasy land': { 'capital':'nowhere', 'population':6.666} }
 
 # Print out the capital of France
 print(europe['france']['population'])
