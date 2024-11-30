4fcf198b5c501b2d0629703b0600b66a14e475b4
Daniel Camacho <danielcamacho0416@gmail.com>
2024-11-30 22:30:15 +0800

    Minor changes

--- DictionariesPrac2.py
+++ DictionariesPrac2.py
@@ -4,6 +4,9 @@ europe = { 'spain': { 'capital':'madrid', 'population':46.77 },
            'germany': { 'capital':'berlin', 'population':80.62 },
            'norway': { 'capital':'oslo', 'population':5.084 },
             'fantasy land': { 'capital':'nowhere', 'population':6.666} }
+manila = { 'tondo': {'tourist spot':'binondo', 'rating':10}, 
+            'sta.mesa': {'tourist spot': 'denlens', 'rating': 10},
+            'intramuros': {'tourist spot:': 'plm', 'rating': 10}}
 
 # Print out the capital of France
 print(europe['france']['population'])
